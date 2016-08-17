3aea1545bff0fab182d68e5ac63613f10651a8d5
Azeem112 <azeemazeem187@gmail.com>
2016-08-17 17:14:13 +0500

    Add files via upload

---
+++ src/a/calculator/ACalculator.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package a.calculator;
+
+/**
+ *
+ * @author STUDENT
+ */
+public class ACalculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Calculator C = new Calculator();
+        C.setVisible(true);
+    }
+    
+}
